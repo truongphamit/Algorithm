e6a230683fdcbdc706fda12dc058abcbadf17952
Pham Quang Truong <truongphamit@users.noreply.github.com>
2015-09-17 09:13:00 +0700

    Update Ex1.cpp

--- Lab1/Ex1.cpp
+++ Lab1/Ex1.cpp
@@ -1,27 +1,34 @@
 #include <iostream>
 using namespace std;
 
+bool isPrimerNumber(int n);
+
 int main() {
     int n;
     int s = 0;
-    cout << "Nhap N: ";
+    cout << "Enter the value for N: ";
     cin >> n;
 
-    if (n <=0 ) {
-        cout << n << " is not primer number";
-    } else {
-        for (int i = 1; i <= n/2; i++) {
-            if (n % i == 0) {
-                s += i;
-            }
-        }
-    }
-
-    if (s == n) {
+    if (isPrimerNumber(n)) {
         cout << n << " is primer number";
     } else {
         cout << n << " is not primer number";
     }
 
 }
+
+bool isPrimerNumber(int n) {
+    bool check = false;
+    int s = 0;
+    if (n > 0) {
+        for (int i = 1; i <= n/2; i++) {
+            if (n % i == 0) {
+                s += i;
+            }
+        }
+        if (s == n) check = true;
+    }
+    return check;
+}
+
 
